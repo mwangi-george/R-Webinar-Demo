10c590ba814c40a2609446afa544a24e654ca401
Mwangi George <mwangigeorge648@gmail.com>
2023-10-03 10:03:44 +0300

    changes

--- Spotify_App.R
+++ Spotify_App.R
@@ -1,33 +1,51 @@
+# Load necessary packages using the 'pacman' package
 pacman::p_load(
   tidyverse, shiny, janitor, shinycssloaders, plotly, shinydashboard, shinyWidgets
 )
 
+# Read a CSV file and clean column names using 'janitor::clean_names()'
 spotify <- read_csv("data/spotify-2023.csv", show_col_types = F) %>% janitor::clean_names()
 
-header <- dashboardHeader(title = "Analysis of Most Streamed Spotify Songs 2023",titleWidth = 1500)
+# Create a dashboard header with a specific title and width
+header <- dashboardHeader(title = "Analysis of Most Streamed Spotify Songs 2023", titleWidth = 1500)
 
+# Create a collapsed dashboard sidebar
 sidebar <- dashboardSidebar(collapsed = T)
 
+# Create a dashboard body with multiple boxes containing plotlyOutput elements
 body <- dashboardBody(
   fluidRow(
+    # Create a box with a plotlyOutput element for "top_10_artists" and add a spinner
     box(width = 6, plotlyOutput("top_10_artists", height = 300) %>% withSpinner()),
+    # Create a box with a plotlyOutput element for "contributions" and add a spinner
     box(width = 6, plotlyOutput("contributions", height = 300) %>% withSpinner()),
+    # Create a box with a plotlyOutput element for "pop_over_time" and add a spinner
     box(width = 6, plotlyOutput("pop_over_time", height = 300) %>% withSpinner()),
+    # Create a box with a plotlyOutput element for "avg_happiness" and add a spinner
     box(width = 6, plotlyOutput("avg_happiness", height = 300) %>% withSpinner()),
+    # Create a box with a plotlyOutput element for "dist" and add a spinner
     box(width = 6, plotlyOutput("dist", height = 300) %>% withSpinner()),
+    # Create a box with a plotlyOutput element for "key_signatures" and add a spinner
     box(width = 6, plotlyOutput("key_signatures", height = 300) %>% withSpinner()),
+    # Create a box with a plotlyOutput element for "splom" and add a spinner
     box(width = 6, plotlyOutput("splom", height = 500) %>% withSpinner()),
+    # Create a box with a plotlyOutput element for "dot" and add a spinner
     box(width = 6, plotlyOutput("dot", height = 500) %>% withSpinner()),
+    # Create a box with a plotlyOutput element for "bpm_vs_dance" and add a spinner
     box(width = 12, plotlyOutput("bpm_vs_dance", height = 500) %>% withSpinner())
   )
 )
 
+# Create the UI for the Shiny dashboard, including the header, sidebar, and body, and specify the skin color
 ui <- dashboardPage(
   header, sidebar, body,
-  skin = "green")
+  skin = "green"
+)
 
+# Define the server function that will generate the dynamic content of the Shiny app
 server <- function(input, output, session){
   
+  # Render a Plotly plot for the top 10 artists with the most songs
   output$top_10_artists <- renderPlotly({
     spotify %>% count(artist_s_name, sort = T) %>% 
       top_n(n = 10, wt = n) %>% 
@@ -39,9 +57,10 @@ server <- function(input, output, session){
         xaxis = list(title = "Artist Name"),
         yaxis = list(title = "Number of Songs")
       )%>%
-      config(displayModeBar = FALSE)
+      config(displayModeBar = FALSE)  # Hide the Plotly display mode bar
   })
   
+  # Render a Plotly plot for the distribution of the number of artists contributing to songs
   output$contributions <- renderPlotly({
     spotify %>% 
       mutate(artist_count = as_factor(artist_count)) %>% 
@@ -53,9 +72,10 @@ server <- function(input, output, session){
         xaxis = list(title = "Number of Artists"),
         yaxis = list(title = "Number of Songs")
       )%>%
-      config(displayModeBar = FALSE)
+      config(displayModeBar = FALSE)  # Hide the Plotly display mode bar
   })
   
+  # Render a Plotly plot to visualize the popularity of songs on Spotify over the years
   output$pop_over_time <- renderPlotly({
     spotify %>% 
       group_by(released_year) %>% 
@@ -71,9 +91,10 @@ server <- function(input, output, session){
         xaxis = list(title = "Year"),
         yaxis = list(title = "Number of Playlists")
       )%>%
-      config(displayModeBar = FALSE)
+      config(displayModeBar = FALSE)  # Hide the Plotly display mode bar
   })
   
+  # Render a Plotly plot to visualize the average happiness score for songs over time
   output$avg_happiness <- renderPlotly({
     spotify %>% 
       dplyr::summarise(
@@ -94,8 +115,10 @@ server <- function(input, output, session){
         xaxis = list(title = "Year", zeroline = T),
         yaxis = list(title = "Happiness Score")
       )%>%
-      config(displayModeBar = FALSE)
+      config(displayModeBar = FALSE)  # Hide the Plotly display mode bar
   })
+  
+  # Render a Plotly plot to visualize the distribution of song streams across the dataset
   output$dist <- renderPlotly({
     spotify %>% 
       mutate(streams = as.numeric(streams)) %>% 
@@ -106,9 +129,10 @@ server <- function(input, output, session){
         xaxis = list(title = "Streams"),
         yaxis = list(title = "Frequency")
       )%>%
-      config(displayModeBar = FALSE)
+      config(displayModeBar = FALSE)  # Hide the Plotly display mode bar
   })
   
+  # Render a Plotly plot to visualize the relationship between song tempo (BPM) and danceability (faceted by key signatures and colored by mode)
   output$bpm_vs_dance <- renderPlotly({
     spotify %>% 
       group_by(key) %>% 
@@ -141,6 +165,7 @@ server <- function(input, output, session){
       subplot(nrows = 3, shareX = T, shareY = T)
   })
   
+  # Render a Plotly plot to visualize the most common key signatures in popular songs
   output$key_signatures <- renderPlotly({
     spotify %>% 
       filter(!is.na(key)) %>% 
@@ -158,9 +183,10 @@ server <- function(input, output, session){
       layout(
         title = "Most common key signatures in popular songs",
         xaxis = list(zeroline = F, title = "% Popularity"), yaxis = list(title = "Key"))%>%
-      config(displayModeBar = FALSE)
+      config(displayModeBar = FALSE)  # Hide the Plotly display mode bar
   })
   
+  # Render a Plotly plot to create a scatter plot matrix
   output$splom <- renderPlotly({
     spotify %>% 
       plot_ly(
@@ -177,9 +203,10 @@ server <- function(input, output, session){
       layout(
         title = "Scatter Plot Matrices"
       ) %>% 
-      config(displayModeBar = F)
+      config(displayModeBar = F)  # Hide the Plotly display mode bar
   })
   
+  # Render a Plotly plot to visualize the most famous key signatures in Spotify playlists
   output$dot <- renderPlotly({
     spotify %>% 
       filter(!is.na(key)) %>% 
@@ -192,8 +219,9 @@ server <- function(input, output, session){
              title = "Most Famous Key Signatures in Spotify Playlists",
              showlegend = F
       ) %>% 
-      config(displayModeBar = F)
+      config(displayModeBar = F)  # Hide the Plotly display mode bar
   })
 }
 
+# Create the Shiny app UI and server
 shinyApp(ui, server)
